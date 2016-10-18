e2914c0f3f45f79dc6f006ec0356724ccef6a869
owner90 <owner90@192.168.5.9>
2016-10-18 13:39:37 +0300

    Пълнене на масив 3х4 и извеждане

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Напишете програма, която позволява на потребителя да попълва масив с
+		// размери 3х4. Изведете масива на екрана.
+
+		Scanner scan = new Scanner(System.in);
+		int[][] massive = new int[3][4];
+
+		for (int i = 0; i < massive.length; i++) {
+			for (int j = 0; j < massive[i].length; j++) {
+				System.out.println("Input number: ");
+				massive[i][j] = scan.nextInt();
+
+			}
+		}
+		for (int i = 0; i < massive.length; i++) {
+			for (int j = 0; j < massive[i].length; j++) {
+				System.out.println(massive[i][j]);
+			}
+	}	
+
+}
+}
